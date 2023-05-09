e582cf9226afc4ca1bf422d816e4788d1c06ab4c
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-05-09 21:19:33 +0300

    .

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+// app.use(router);
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'Main',
+  })
+  .then(() => {
+    console.log('Connected to the Database.');
+  })
+  .catch((err) => console.error(err));
+
+app.listen(PORT, console.log(`server is listening on port: ${PORT}`));
